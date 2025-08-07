8620f931ddf8a61dfd1300cbc0ee9e803960d4df
workkouka <letsgojieun@gmail.com>
2025-08-07 19:36:39 +0900

    :sparkles: feat: add useScrollToElement hook for smooth scrolling

---
+++ src/hooks/useScrollToElement.ts
@@ -0,0 +1,43 @@
+import { Project } from "@/types/portfolio";
+import { useRef, useEffect } from "react";
+
+interface UseScrollToElementProps {
+  isDesktop: boolean;
+  dependency?: Project | null;
+}
+
+export function useScrollToElement({
+  isDesktop,
+  dependency,
+}: UseScrollToElementProps) {
+  const topRef = useRef<HTMLDivElement>(null);
+  const targetRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (dependency) {
+      const timeoutId = setTimeout(() => {
+        if (isDesktop) {
+          window.scrollTo({ top: 0, behavior: "smooth" });
+          if (topRef.current) {
+            topRef.current.scrollIntoView({
+              behavior: "smooth",
+              block: "start",
+            });
+          }
+        } else {
+          if (targetRef.current) {
+            targetRef.current.scrollIntoView({
+              behavior: "smooth",
+              block: "start",
+              inline: "nearest",
+            });
+          }
+        }
+      }, 150);
+
+      return () => clearTimeout(timeoutId);
+    }
+  }, [dependency, isDesktop]);
+
+  return { topRef, targetRef };
+}
